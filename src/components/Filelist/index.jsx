65167bab62629afacc818c1c25849a287ba89b1f
thedmj <dmj1986126@163.com>
2016-11-06 22:58:20 +0800

    update

--- src/components/Filelist/index.jsx
+++ src/components/Filelist/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ajax from "./ajax.js";
-import { Icon } from "antd";
-import { Router, Route, hashHistory } from 'react-router';
+import { Icon ,Breadcrumb} from "antd";
+import { Router, Route, hashHistory ,Link} from 'react-router';
 import Loading from "../Loading";
 import $ from "jquery";
 require("./index.css");
 require("antd/dist/antd.min.css");
-// var TimelineMax = require("../../../gsap/TweenMax.js");
 import TweenMax from "../../../gsap/TweenMax.js";
 
 var Filelistitem = React.createClass({
@@ -24,7 +23,7 @@ var Filelistitem = React.createClass({
     },
 
 });
-
+var url = "http://101.200.129.112:9527/static/";
 var Filelist = React.createClass({
     getInitialState: function () {
         return { dir: [], path: "/"+this.props.routeParams.splat ,loading:false}
@@ -48,34 +47,52 @@ var Filelist = React.createClass({
         var items = dir.map(function (item, index) {
             return <Filelistitem key={item + index} title={item.name} itemclick={This.itemClickHandle} path={item.path} isfolder={item.isFolder}/>
         });
-        console.log();
+        var breadItems =[];
+        var breadPath="";
+        breadItems = this.props.params.splat.split("/").map(function (o,index){
+            breadPath+="/"+o;
+            return (
+                <Breadcrumb.Item key={o+index}><Link to={breadPath}>{o}</Link></Breadcrumb.Item>
+            )
+        });
+        
         return (
-            <div className="filelist" >
-            <h1><span>C</span><span>L</span><span>O</span><span>U</span><span>N</span><span>D</span><span>云</span><span>盘</span></h1>
+            <div className="filelist" onContextMenu={(e)=>{e.preventDefault();}}>
+                
+                <h1><span>C</span><span>L</span><span>O</span><span>U</span><span>N</span><span>D</span><span>云</span><span>盘</span></h1>
+                <div className="bread">
+                    <Breadcrumb>
+                        <Breadcrumb.Item><Link to="/">Home</Link></Breadcrumb.Item>
+                        {breadItems}
+                    </Breadcrumb>
+                </div>
                 <ul style={{"WebkitFilter":this.state.loading?"blur(6px)":"blur(0px)"}}>
                     {items}
                 </ul>
                 <Loading show={this.state.loading} />
+
             </div>
         )
     },
     itemClickHandle: function (path,isfolder) {
         if(isfolder){
-            this.update(path,true);
+            hashHistory.push(path);
         }else{
-            // hashHistory.push(path);
-            window.open(path);
+            window.open(url+ path);
+            console.log(path);
         }
         
     },componentWillReceiveProps: function(nextProps) {
-        var path ="/"+nextProps.routeParams.splat;
-        this.update(path,false);
+        var path =nextProps.routeParams.splat;
+        if(this.props.routeParams.splat!== path){
+            this.update(path);
+        }
+        console.log(this.props.routeParams.splat);
     },
-    update:function(path,needPush){
+    update:function(path){
         this.setState({loading:true});
         var This = this;
         ajax(path, function (res) {
-            if(needPush){hashHistory.push(path);}
             This.setState({ dir: res.file, path: path,loading:false });
         }, function (err) {
             console.log(err);
