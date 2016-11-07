04cd3623fce4e1f586e106a31b801117d7ccb040
thedmj <dmj1986126@163.com>
2016-11-07 19:00:48 +0800

    update

--- src/components/Filelist/index.jsx
+++ src/components/Filelist/index.jsx
@@ -1,32 +1,98 @@
 import React from 'react';
+// import ReactDOM from 'react-dom';
 import ajax from "./ajax.js";
-import { Icon ,Breadcrumb} from "antd";
+import { Icon ,Breadcrumb,Input} from "antd";
 import { Router, Route, hashHistory ,Link} from 'react-router';
 import Loading from "../Loading";
 import $ from "jquery";
+import Menu from "../Menu";
 require("./index.css");
-require("antd/dist/antd.min.css");
+require("antd/dist/antd.css");
 import TweenMax from "../../../gsap/TweenMax.js";
+import request from "superagent";
 
 var Filelistitem = React.createClass({
+    getInitialState:function(){
+        return {
+            showRename:false,
+            title:this.props.title
+        }
+        
+    },
     render: function () {
+        var style={
+            background:this.props.active?"blue":"inherit"
+        }
+        var typeList={".html":"code",".txt":"file-text",".jpg":"file-jpg",".png":"picture",".git":"picture",".md":"book"};
+        
+        var type = typeList[this.props.ext];
+        if(!typeList[this.props.ext]){
+            type="file-unknown"
+        };
         return (
-            <li onClick={(path,isfolder) => { this.props.itemclick(this.props.path,this.props.isfolder); } }>
-                <span className="icon">
-                    <Icon type={this.props.isfolder?"folder":"link"} />
+            <li  onMouseDown={this.rightClick} style={style}>
+            
+                <span className="icon" onClick={(path,isfolder) => { this.props.itemclick(this.props.path,this.props.isfolder); } }>
+                    <Icon type={this.props.isfolder?"folder":type} />
                 </span>
-                <span className="title">
-                    {this.props.title}
+                <span className="title" style={{display:this.state.showRename?"none":"inline-block"}}>
+                    {this.state.title}
                 </span>
+                <div className="rename" style={{display:this.state.showRename?"block":"none"}}>
+                    <Input type="text" defaultValue={this.props.title} ref={this.props.title} onMouseDown={(e)=>{e.stopPropagation();}} onPressEnter={this.onPressEnter}/>
+                </div>
             </li>
         )
     },
-
+    onPressEnter:function(e){ //rename输入回车
+        this.rename(this.props.path,e.target.value);
+    },
+    rename:function(path,name){ //重命名函数
+        var url = "http://101.200.129.112:9527/file/rename/";
+        var This = this;
+        request.get(url).query({
+            path:path,
+            name:name
+        }).end(function(err,res){
+            if(err){console.log(err)}
+            if(res.ok){
+                This.props.cancel();
+                This.setState({
+                    title:name
+                });
+            }
+        });
+    },
+    componentDidUpdate:function(){
+        if(this.props.pickItemName === this.props.title){
+            
+            
+        }
+    },
+    componentWillReceiveProps:function(nextProps){ //更换选中项后取消激活rename 这里componentWillReceiveProps会遍历items
+        if(this.props.selectedItem.name===""){
+            this.setState({
+                showRename:false
+            });
+        }
+        
+    },
+    
+    rightClick:function(e){ //右键文件激活选中项
+        if(e.button === 2){
+            this.props.pickItem(this.props.title,this);
+        }
+    },
+    
 });
 var url = "http://101.200.129.112:9527/static/";
+
+// ------------------------------------------------------------------------------------
+
+
 var Filelist = React.createClass({
     getInitialState: function () {
-        return { dir: [], path: "/"+this.props.routeParams.splat ,loading:false}
+        return { dir: [], path: "/"+this.props.routeParams.splat ,loading:false,selectedItem:{name:"",item:null},showMenu:false,position:{x:0,y:0}}
     },
     componentDidMount: function () {
         this.setState({loading:true});
@@ -45,7 +111,12 @@ var Filelist = React.createClass({
         var This = this;
         var dir = this.state.dir;
         var items = dir.map(function (item, index) {
-            return <Filelistitem key={item + index} title={item.name} itemclick={This.itemClickHandle} path={item.path} isfolder={item.isFolder}/>
+            var active = false;
+            var renameShow =false;
+            if(item.name === This.state.selectedItem.name){
+                active = true;
+            }
+            return <Filelistitem  renameShow={renameShow} active={active} key={item.path+item.name} title={item.name} itemclick={This.itemClickHandle} path={item.path} ext={item.ext} isfolder={item.isFolder} pickItem={This.pickItem} pickItemName={This.state.selectedItem.name} selectedItem={This.state.selectedItem} cancel={This.cancel}/>
         });
         var breadItems =[];
         var breadPath="";
@@ -57,8 +128,7 @@ var Filelist = React.createClass({
         });
         
         return (
-            <div className="filelist" onContextMenu={(e)=>{e.preventDefault();}}>
-                
+            <div className="filelist" onContextMenu={(e)=>{e.preventDefault();}} onMouseDown={this.rightClick}>
                 <h1><span>C</span><span>L</span><span>O</span><span>U</span><span>N</span><span>D</span><span>云</span><span>盘</span></h1>
                 <div className="bread">
                     <Breadcrumb>
@@ -70,26 +140,25 @@ var Filelist = React.createClass({
                     {items}
                 </ul>
                 <Loading show={this.state.loading} />
-
+                <Menu  position={this.state.position} showMenu={this.state.showMenu} onrename={this.renameShow} selectedItemName={this.state.selectedItem.name}/>
             </div>
         )
     },
-    itemClickHandle: function (path,isfolder) {
+    itemClickHandle: function (path,isfolder) { //文件点击事件
         if(isfolder){
             hashHistory.push(path);
         }else{
             window.open(url+ path);
-            console.log(path);
+            
         }
         
-    },componentWillReceiveProps: function(nextProps) {
+    },componentWillReceiveProps: function(nextProps) { //改变路由时获取路径来改变state
         var path =nextProps.routeParams.splat;
         if(this.props.routeParams.splat!== path){
             this.update(path);
         }
-        console.log(this.props.routeParams.splat);
     },
-    update:function(path){
+    update:function(path){ //请求函数
         this.setState({loading:true});
         var This = this;
         ajax(path, function (res) {
@@ -97,9 +166,48 @@ var Filelist = React.createClass({
         }, function (err) {
             console.log(err);
         });
-    }
+    },
+
+    pickItem:function(filename,file){ //获取选中项
+        this.setState({
+            selectedItem:{
+                name:filename,
+                item:file
+            }
+        });
+    },
+    rightClick:function(e){ //右键弹出菜单 左键隐藏
+        if(e.button ===2){
+            this.setState({
+                showMenu:true,
+                position:{
+                    x:e.clientX,
+                    y:e.clientY
+                }
+            });
+        }else{
+            this.cancel();
+        }
+    },
+    cancel:function(){
+        this.setState({
+            showMenu:false,
+            selectedItem:{
+                name:"",
+                item:null
+            }
+        });
+    },
+    renameShow:function(){ //激活重命名
+        this.state.selectedItem.item.setState({
+            showRename:true
+        })
+
+    },
+    
 
 });
+
 var R = React.createClass({
     render: function () {
         return (
---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./index.css";
+var Menu =React.createClass({
+
+    render:function(){
+        var style={
+            display:this.props.showMenu?"block":"none",
+            left:this.props.position.x,
+            top:this.props.position.y
+        }
+        return (
+            <div className="menu" style={style} >
+                <ul>
+                    <li style={{display:!!this.props.selectedItemName?"none":"block"}}>新建文件夹</li>
+                    <li style={{display:!!this.props.selectedItemName?"block":"none"}}>复制</li>
+                    <li style={{display:!!this.props.selectedItemName?"block":"none"}}>剪切</li>
+                    <li style={{display:!!this.props.selectedItemName?"block":"none"}}>粘贴</li>
+                    <li style={{display:!!this.props.selectedItemName?"block":"none"}}>删除</li>
+                    <li style={{display:!!this.props.selectedItemName?"block":"none"}} onMouseDown={this.props.onrename}>重命名</li>
+                </ul>
+            </div>
+        );
+    },
+    
+});
+module.exports = Menu;
